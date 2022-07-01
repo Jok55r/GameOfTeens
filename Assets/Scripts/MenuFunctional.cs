f5f876c472f0636a6d62e4669f0093878abecdb5
IceStorman <vladislavmartinisin@gmail.com>
2022-07-02 00:21:43 +0300

    Added menu

---
+++ Assets/Scripts/MenuFunctional.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuFunctional : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+
+    }
+}
