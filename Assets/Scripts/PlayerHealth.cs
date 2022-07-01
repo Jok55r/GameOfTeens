f9cd72bd5bd88fd1c1f5f6665020b91ae457297b
Jok55r <81425160+Jok55r@users.noreply.github.com>
2022-07-01 22:09:42 +0200

    yes

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    /*public Image hpBar;
+    public int hp = 100;
+
+    void Start()
+    {
+        hpBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        hpBar.fillAmount = (float)(hp / 100);
+    }*/
+}
