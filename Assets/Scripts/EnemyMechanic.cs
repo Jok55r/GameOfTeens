61c73e78eedf81e07c4b2905f57f50f9be61594a
IceStorman <vladislavmartinisin@gmail.com>
2022-07-02 18:51:48 +0300

    Added enemy shoot mechanic

--- Assets/Scripts/EnemyMechanic.cs
+++ Assets/Scripts/EnemyMechanic.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class EnemyMechanic : MonoBehaviour
 {
     [SerializeField] private float range;
-    [SerializeField] private Transform target;
-    [SerializeField] private bool detected;
-    [SerializeField] private Vector2 direction;
+    [SerializeField] private Transform targetPlayer;
+    [SerializeField] private Transform targetVan;
+    [SerializeField] private Movement movement;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float fireRate;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float force;
+    private Vector2 direction;
+    private float nextTimeToFire = 0;
+    private bool detected;
 
     private void Update()
     {
-        Vector2 targetPos = target.position;
+        //Vector2 targetPos = movement.canMove ? targetPlayer.position : targetVan.position;
+        Vector2 targetPos = targetVan.position;
         direction = targetPos - (Vector2)transform.position;
         RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
 
@@ -32,6 +40,20 @@ public class EnemyMechanic : MonoBehaviour
                 }
             }
         }
+        if (detected)
+        {
+            if(Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletIns = Instantiate(bullet, shootPoint.position, Quaternion.identity);
+        bulletIns.GetComponent<Rigidbody2D>().AddForce(direction * force);
     }
 
     private void OnDrawGizmosSelected()
