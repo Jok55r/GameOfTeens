f540cdd464c1bfd97aaa544418eb74bec8189ec3
IceStorman <vladislavmartinisin@gmail.com>
2022-07-02 22:17:34 +0300

    Added settings functional

--- Assets/Scripts/MenuFunctional.cs
+++ Assets/Scripts/MenuFunctional.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuFunctional : MonoBehaviour
 {
+    [SerializeField] private AudioMixerGroup mixer;
+
+    [SerializeField] private Slider masterSld;
+    [SerializeField] private Slider musicSld;
+    [SerializeField] private Slider SFXSld;
+
     [SerializeField] private GameObject tutorialPanel;
     [SerializeField] private AudioSource clickEffect;
         
@@ -37,6 +45,21 @@ public class MenuFunctional : MonoBehaviour
     {
         clickEffect.Play();
         panel.SetActive(false);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        mixer.audioMixer.SetFloat("MasterVolume", masterSld.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        mixer.audioMixer.SetFloat("MusicVolume", musicSld.value);
+    }
+
+    public void ChangeSFXVolume()
+    {
+        mixer.audioMixer.SetFloat("SFXVolume", SFXSld.value);
     }
 
     public void QuitGame()
