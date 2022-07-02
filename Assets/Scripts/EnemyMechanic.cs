9fde6a92c60cac84804e47544579f77f6ec04a42
IceStorman <vladislavmartinisin@gmail.com>
2022-07-02 17:17:49 +0300

    Some shoot changes

--- Assets/Scripts/EnemyMechanic.cs
+++ Assets/Scripts/EnemyMechanic.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class EnemyMechanic : MonoBehaviour
 {
-    
+    [SerializeField] private float range;
+    [SerializeField] private Transform target;
+    [SerializeField] private bool detected;
+    [SerializeField] private Vector2 direction;
+
+    private void Update()
+    {
+        Vector2 targetPos = target.position;
+        direction = targetPos - (Vector2)transform.position;
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
+
+        if (rayInfo)
+        {
+            if(rayInfo.collider.gameObject.tag == "Player")
+            {
+                if(detected == false)
+                {
+                    detected = true;
+                }
+            }
+            else
+            {
+                if(detected == true)
+                {
+                    detected = false;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
 }
